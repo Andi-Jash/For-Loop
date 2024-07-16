39905573100d27c6bd172c763e51e1c9bf42d66f
Andi Jashari/Kurs <an0nymouscrature@gmail.com>
2024-07-16 12:16:26 +0200

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,13 @@
+emri = "Andi"
+for i in emri:
+    print(i)
+
+for letter in "Python":
+    if letter == "h":
+       break
+    print("shkronjat qe i kemi mar:", letter)
+
+for letter in "Python":
+    if letter == "h":
+        continue
+    print("shkronjat qe i kemi mar:",letter)
